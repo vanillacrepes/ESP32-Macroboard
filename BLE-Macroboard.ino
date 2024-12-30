f93103589dac02dbe598d00df71fefce2d188b68
vanillacrepes <daveplayzminecraft@gmail.com>
2024-12-30 17:16:06 +0800

    Working BLE and OLED Display

---
+++ BLE-Macroboard.ino
@@ -0,0 +1,63 @@
+// Include Packages
+// OLED
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+// BLE Keyboard
+#include <BleKeyboard.h>
+
+// Pin adresses (Left to right on board)
+int a = 15;
+int b = 18;
+int c = 4;
+int d = 19;
+
+// Variables
+string states[4] = {"HIGH", "HIGH", "HIGH", "HIGH"};
+
+// Run once on awake
+void setup() {
+  // Start Serial Monitor
+  Serial.begin(115200);
+  Serial.println("Starting BLE!");
+
+  // Start Keyboard
+  bleKeyboard.begin();
+
+  // Initialize Display
+  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR); //Not even gonna pretend like I know what this does.
+  display.clearDisplay
+  display.setTextColor(PINK);
+  display.textSize(2)
+  display.setCursor(20, 10);
+  display.println("NERV");
+  display.textSize(1);
+  display.println("God is in his heaven, all is right with the world");
+  
+  // Set Pin modes
+  pinMode(a, INPUT_PULLUP);
+  pinMode(b, INPUT_PULLUP);
+  pinMode(c, INPUT_PULLUP);
+  pinMode(d, INPUT_PULLUP);
+}
+
+void loop() {
+  // Read button states
+  states[0] = digitalRead(a);
+  states[1] = digitalRead(b);
+  states[2] = digitalRead(c);
+  states[3] = digitalRead(d);
+
+  // If a button state is LOW, it's pressed
+  if(states[0] == LOW) {
+    Serial.println(0)
+  }
+  if(states[1] == LOW) {
+    Serial.println(1)
+  }
+  if(states[2] == LOW) {
+    Serial.println(2)
+  }
+  if(states[3] == LOW) {
+    Serial.println(3)
+  }
+}
